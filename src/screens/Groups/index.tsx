aca04a6855dd88b2d933d372cbc7aef6fd676f87
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2022-10-14 21:37:20 -0300

    Componente List Empty

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,11 +4,12 @@ import { FlatList } from 'react-native';
 import { Header } from '@components/Header';
 import { HighLight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
+import { ListEmpty } from '@components/ListEmpty';
 
 import { Container } from './styles';
 
 export function Groups() {
-  const [groups, setGroups] = useState<string[]>(['Galera da Rocket']);
+  const [groups, setGroups] = useState<string[]>([]);
 
   return (
     <Container>
@@ -26,6 +27,10 @@ export function Groups() {
           <GroupCard
             title={item}
           />
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
         )}
       />
     </Container>
